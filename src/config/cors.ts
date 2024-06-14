fe7e531b5ad6814bc1d7ced5c25a2706a597fadf
Ivan <ivan.sosatovar@gmail.com>
2024-06-13 21:46:48 -0700

    scrip en package.json

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,22 @@
-import { CorsOptions } from 'cors';
+import express from 'express';
+import cors from 'cors';
 
-export const corsConfig: CorsOptions = {
-    origin: '*',
-  };
+const app = express();
+
+// Configuración de CORS para permitir todas las solicitudes
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
+
+// Resto de la configuración de tu aplicación Express
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Iniciar el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
