ff69dc439b45cd736402efeaffaa66fecf079ef1
Ivan <ivan.sosatovar@gmail.com>
2024-06-13 21:33:58 -0700

    scrip en package.json

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,14 +1,5 @@
 import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
-    // Permitir todas las solicitudes si se pasa el argumento '--api'
-    if (process.argv.includes('--api')) {
-      callback(null, true);
-      return;
-    }
-
-    // Permitir todas las solicitudes desde cualquier origen
-    callback(null, true);
-  }
-};
+    origin: '*',
+  };
